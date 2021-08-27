7aab23ca11f9dfdc83646c980f9185edff243b9c
Elmeri Väisänen <elmeri.vaisanen@itvelhot.local>
2021-08-27 13:28:24 +0300

    paljon muutoksia

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include_once 'layout/top.inc.php'; ?>
+<?php include_once 'layout/nav.inc.php'; ?>
+      
+
+    
+    <div class="list-group">
+        <a href="#" class="list-group-item list-group-item-action active">Welcome to VoteApp</a>
+      </div>
+
+      <?php include_once 'layout/bottom.inc.php'; ?>
---
+++ login.php
@@ -0,0 +1,22 @@
+<?php include_once 'layout/top.inc.php'; ?>
+<?php include_once 'layout/nav.inc.php'; ?>
+
+<div class="container"></div>
+
+<form>
+    <fieldset>
+      <legend>Login</legend>
+      <div class="form-group">
+        <label for="exampleInputEmail1" class="form-label mt-4">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1" class="form-label mt-4">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </fieldset>
+  </form>
+</div>
+        <?php include_once 'layout/bottom.inc.php'; ?>
