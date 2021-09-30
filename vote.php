0e7361268f65b922c0b8a092dade18e34e2195e4
Elmeri Väisänen <elmeri.vaisanen@itvelhot.local>
2021-09-30 16:09:05 +0300

    eteenpäin

--- vote.php
+++ vote.php
@@ -13,17 +13,21 @@ $id = intval($_GET['id']);
 
 <div class="container">
 
-    <h1>Kuka on nopein?</h1>
+    <h1></h1>
 
-    <ul class="list-group">
-        <li class="list-group-item"><button class="btn btn-lg btn-info">Auto</button></li>
-        <li class="list-group-item"><button class="btn btn-lg btn-info">Lentokone</button></li>
-        <li class="list-group-item"><button class="btn btn-lg btn-info">Usain Bolt</button></li>
+    <ul id="optionsUl" class="list-group">
+        
     </ul>
+
+<!-- 
+    <li class="list-group-item"><button class="btn btn-lg btn-info">Auto</button></li>
+    <li class="list-group-item"><button class="btn btn-lg btn-info">Lentokone</button></li>
+    <li class="list-group-item"><button class="btn btn-lg btn-info">Usain Bolt</button></li> 
+-->
 
 </div>
 
-<script src="js/vote.js"></script>
 <script src="js/common.js"></script>
+<script src="js/vote.js"></script>
 
 <?php include_once 'layout/bottom.inc.php'; ?>
